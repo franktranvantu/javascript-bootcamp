9a4fd1d111707dee9cac6b747cac9ad466aebf41
frank <franktranvantu@gmail.com>
2020-12-22 23:23:58 +0700

    update

---
+++ 10-how-template-literals-improve-strings/index.js
@@ -0,0 +1,17 @@
+let username = "Jane Doe";
+let message = `Hi ${username}, how are you?`;
+// console.log(message);
+
+// template literal ``
+
+// const weight = 150;
+// const moonWeight = `You weigh ${weight * 0.165} pounds on the moon`;
+// console.log(moonWeight);
+
+// `I'm a string`;
+// `He said, "I am string."`;
+
+const threeLines = `This is a string 
+that spans across 
+three lines.
+`;
---
+++ 12-how-variables-should-be-named/index.js
@@ -0,0 +1,19 @@
+// const value1 = 'Frank';
+// const value2 = 'Tran';
+// const newValue = `${value1} ${value2}`;
+// console.log(newValue);
+
+let firstName = 'Frank';
+let lastName = 'Tran';
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+// const modalVisible = true;
+let isModalVisible = true;
+let hasPosition = false;
+
+if (isModalVisible) {
+    // do something
+}
+
+const RED_COLOR = '#f00';
